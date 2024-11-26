055ce5fa3b83a7b50477dc8cd2de02bd08ca2466
Daniel <carlosdanielledesma76@gmail.com>
2024-11-26 17:54:20 +0100

    Se modificcomponente PaginationApp con nuevos estilos

--- src/components/PaginationApp.jsx
+++ src/components/PaginationApp.jsx
@@ -10,14 +10,6 @@ for(let i=1; i<= Math.ceil(totalProducts/productsPerPage); i++){
     pageNumbers.push(i)
 }
 
-const onPreviusPage = ()=>{
-    setCurrentPage(currentPage-1)
-}
-
-const onNextPage = ()=>{
-    setCurrentPage(currentPage+1)
-}
-
 const onSpecificPage = (n)=>{
     setCurrentPage(n)
 }
@@ -29,22 +21,19 @@ const onSpecificPage = (n)=>{
         role="navigation" 
         aria-label="pagination"
     >
-        <a className={`pagination-previous ${currentPage === 1 ? 'is-disabled' : ''}`} onClick={onPreviusPage}>Anterior</a>
-        <a className={`pagination-next ${currentPage >= pageNumbers.length ? 'is-disabled' : ''}`} onClick={onNextPage}>Siguiente</a>
         <ul className="pagination-list m-1">
             
             {pageNumbers.map(noPage =>(
                     
                     <li id="paginacion" key={noPage}>
                         <a  className={`pagination-link ${
-                            noPage=== currentPage ? 'is-current' : '' 
+                            noPage === currentPage ? 'is-current custom-highlight' : 'custom-default' 
                             }`}
                             onClick={()=> onSpecificPage(noPage)}
                         >
                             {noPage}
                         </a>
-                    </li>
-                    
+                    </li>                   
             ))}            
         </ul>
     </nav>
